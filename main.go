f907bd8e9d279cb0ffc4ff611aa9bd595df28f80
thekalugin <thekalugin@gmail.com>
2025-07-14 18:45:49 +0300

    add main/account

--- main.go
+++ main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
+	"passwords/account"
 
 	"github.com/fatih/color"
 )
 
 func main() {
-	fmt.Println("start")
-	color.Red("start")
+	login := promptData("Enter your login")
+	password := promptData("Enter your password")
+	url := promptData("Enter URL")
+	myAccount, err := account.NewAccountWithTimeStamp(login, password, url)
+	if err != nil {
+		if err.Error() == "INVALID_LOGIN" {
+			color.Red("Error! Login cannot be empty")
+		} else if err.Error() == "INVALID_URL" {
+			color.Red("Error! Invalid URL format")
+		}
+	}
+	myAccount.OutputData()
+}
+
+func promptData(prompt string) string {
+	fmt.Print(prompt + ": ")
+	var res string
+	fmt.Scanln(&res)
+	return res
 }
